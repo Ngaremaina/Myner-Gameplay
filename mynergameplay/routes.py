9e756dcd4bc4a640624b6fc0bcd983301c3f26ea
Ngare <45742522+Ngaremaina@users.noreply.github.com>
2023-02-17 00:47:26 +0300

    Myner Gameplay

--- mynergameplay/routes.py
+++ mynergameplay/routes.py
@@ -4,3 +4,11 @@ from flask import render_template
 @app.route("/")
 def home():
     return render_template('home.html')
+
+@app.route("/playlist")
+def playlist():
+    return render_template('playlist.html')
+
+@app.route("/videos")
+def videos():
+    return render_template('video.html')
