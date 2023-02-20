2aee3e8f360981ddaf2f6a01144f8742fb9df446
Ngare <45742522+Ngaremaina@users.noreply.github.com>
2023-02-20 07:32:41 +0300

    Myner Gameplay

--- mynergameplay/routes.py
+++ mynergameplay/routes.py
@@ -1,5 +1,8 @@
 from mynergameplay import app
 from flask import render_template
+from mynergameplay.forms import ContactForm
+from flask import request
+import pandas as pd
 
 @app.route("/")
 def home():
@@ -21,6 +24,18 @@ def about():
 def privacy():
     return render_template('privacy.html')
 
-@app.route("/contact us")
+@app.route("/contact us", methods=["GET","POST"])
 def contact():
-    return render_template('contact.html')
+    form = ContactForm()
+    # here, if the request type is a POST we get the data on contat
+    #forms and save them else we return the contact forms html page
+    if request.method == 'POST':
+        name =  request.form["name"]
+        email = request.form["email"]
+        subject = request.form["subject"]
+        message = request.form["message"]
+        res = pd.DataFrame({'name':name, 'email':email, 'subject':subject ,'message':message}, index=[0])
+        res.to_csv('./contactusMessage.csv')
+        print("The data are saved !")
+    else:
+        return render_template('contact.html', form=form)
